3513ced873c4761aefa6fb062023dae130074304
nBurich <n.burich@hotmail.com>
2019-04-27 00:19:00 -0400

    beginning

---
+++ assets/javascript/train.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messageSenderId: "",
+};
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    
